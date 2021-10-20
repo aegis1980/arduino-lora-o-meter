d38f87a0a9e4f2f94c8692cdb3e9502e4e41f3ee
aegis1980 <jonrobinson1980@gmail.com>
2021-10-20 17:30:10 +1300

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
-// gps stuff
+// gps stuff & compass
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
 
 // lcd stuff
 #include <LiquidCrystal.h>
@@ -9,23 +12,21 @@
 #include <SPI.h>
 #include <RH_RF95.h>
 
+//button stuff
+
+
 // gps stuff
 static const int RXPin = 4, TXPin = 3;
 static const uint32_t GPSBaud = 9600;
 
+// Assign a Unique ID to the HMC5883 Compass Sensor
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+
 // The TinyGPS++ object
 TinyGPSPlus gps;
 
-
 // The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
-
-TinyGPSCustom totalGPGSVMessages(gps, "GPGSV", 1); // $GPGSV sentence, first element
-TinyGPSCustom messageNumber(gps, "GPGSV", 2);      // $GPGSV sentence, second element
-TinyGPSCustom satNumber[4]; // to be initialized later
-TinyGPSCustom elevation[4];
-bool anyChanges = false;
-unsigned long linecount = 0;
 
 // lcd stuff
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
@@ -39,8 +40,90 @@ char *node_id = "<1531236>";  //From LG01 via web Local Channel settings on MQTT
 uint8_t datasend[36];
 unsigned int count = 1;
 
+// Draguino vrsion of Radiohead implements rf95.setSyncWord()
+void setSyncWord(int sw){
+  rf95.spiWrite(RH_RF95_REG_39_SYNC_WORD,sw);
+}
+
 // lora-a-meter stuff
 float lat,lng,rssi;
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  mag.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" uT");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" uT");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" uT");  
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+
+void displayGpsInfo()
+{
+  // Prints the location if lat-lng information was recieved
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  // prints invalid if no information was recieved in regards to location.
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  // prints the recieved GPS module date if it was decoded in a valid response.
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    // prints invalid otherwise.
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  // prints the recieved GPS module time if it was decoded in a valid response.
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    // Print invalid otherwise.
+    Serial.print(F("INVALID"));
+  }
+  Serial.println();
+  if(mag.begin())
+  {
+    //displayCompassInfo();
+  }
+}
 
 void setup() {
   Serial.begin(9600);
@@ -61,9 +144,13 @@ void setup() {
   
   rf95.setFrequency(frequency);
   rf95.setTxPower(13);
-  rf95.setSyncWord(0x34);
+  setSyncWord(0x34);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+   // This sketch displays information every time a new sentence is correctly encoded from the GPS Module.
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+      displayGpsInfo();
 }
